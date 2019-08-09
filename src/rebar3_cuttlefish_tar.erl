d02e279be8798fa4bf16c15c9df75241fec6326d
Martin Cox <martin@cox.dev>
2019-08-09 15:57:13 +0100

    Add tar provider.

--- src/rebar3_cuttlefish_tar.erl
+++ src/rebar3_cuttlefish_tar.erl
@@ -1,37 +1,36 @@
 -module(rebar3_cuttlefish_tar).
+
 -behaviour(provider).
 
--export([init/1
-        ,do/1
-        ,format_error/1]).
+-export([init/1,
+         do/1,
+         format_error/1]).
 
 -define(PROVIDER, tar).
 -define(NAMESPACE, default).
--define(DEPS, [{?NAMESPACE, release}]).
 
 %% ===================================================================
 %% Public API
 %% ===================================================================
+
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    Provider = providers:create([
-            {name, ?PROVIDER},            % The 'user friendly' name of the task
-            {module, ?MODULE},            % The module implementation of the task
-            {namespace, ?NAMESPACE},
-            {bare, true},                 % The task can be run by the user, always true
-            {deps, ?DEPS},                % The list of dependencies
-            {example, "rebar3 cuttlefish tar"}, % How to use the plugin
-            {opts, relx:opt_spec_list()},                   % list of options understood by the plugin
-            {short_desc, "Rebar3 cuttlefish release tarball plugin"},
-            {desc, ""}
-    ]),
-    {ok, rebar_state:add_provider(State, Provider)}.
-
+    State1 = rebar_state:add_provider(State, 
+                                      providers:create([{name, ?PROVIDER},
+                                        {module, ?MODULE},
+                                        {namespace, ?NAMESPACE},
+                                        {bare, true},
+                                        {example, "rebar3 tar"},
+                                        {short_desc, "Tar archive of release built of project."},
+                                        {desc, "Tar archive of release built of project."},
+                                        {opts, relx:opt_spec_list()}])),
+    {ok, State1}.
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
     rebar_relx:do(rlx_prv_release, "tar", ?PROVIDER, State).
 
--spec format_error(any()) ->  iolist().
-format_error(Error) ->
-    io_lib:format("~p", [Error]).
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
+
